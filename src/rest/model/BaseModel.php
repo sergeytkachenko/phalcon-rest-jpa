61720723b3f28973b59610c3e5ef0acbb677a376
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2016-03-25 00:58:00 +0200

    added base joinedRelations

--- src/rest/model/BaseModel.php
+++ src/rest/model/BaseModel.php
@@ -1,10 +1,13 @@
 <?
 namespace PPA\Rest\Model;
+use Phalcon\Exception;
+use Phalcon\Mvc\Model;
+use Phalcon\Mvc\Model\Relation;
 
 /**
  * Class BaseModel
  * Служит базовым вспомагательным "класом" для потдержки общих методов моделей
-*/
+ */
 trait BaseModel
 {
 
@@ -13,36 +16,98 @@ trait BaseModel
 	 * @var array
 	 */
 	public $relations = array();
+	public $joinedRelations = array();
+	public static $referencesKeyOption = 'key';
+
+	/**
+	 * Returns all relations of model.
+	 * @return Relation[]
+	 */
+	public function getRelations() {
+		return $this->getModelsManager()->getRelations(get_class($this));
+	}
 
 	/**
 	 * Подгружает все связанные свойства из других таблиц и ложит все связи в свойство $this->relations
 	 * Очень удобно когда вам нужно выбрать один методом сущность, а также все ее связи
+	 * @param bool $isConvertToArray
 	 * @return $this
 	 */
-	public function fetchRelations() {
-		$relations = $this->getModelsManager()->getBelongsTo($this);
+	public function fetchRelations($isConvertToArray = true) {
+		$relations = $this->getRelations();
 		foreach ($relations as $relation) {
-			$this->assignRelation($relation);
-		}
-		$relations = $this->getModelsManager()->getHasMany($this);
-		foreach ($relations as $relation) {
-			$this->assignRelation($relation);
-		}
-		$relations = $this->getModelsManager()->getHasManyToMany($this);
-		foreach ($relations as $relation) {
-			$this->assignRelation($relation);
+			$this->assignRelation($relation, $isConvertToArray);
 		}
 		return $this;
 	}
 
 	/**
+	 * @throws Exception
+	 */
+	public function joinedRelations() {
+		$relations = $this->getRelations();
+		foreach ($relations as $relation) {
+			$this->joinedRelation($relation);
+		}
+	}
+
+	/**
+	 * @param \Phalcon\Mvc\Model\Relation $relation
+	 * @throws Exception
+	 */
+	public function joinedRelation($relation) {
+		$keyField = $relation->getOption(self::$referencesKeyOption);
+		$relationAlias = $relation->getOption('alias');
+		$relationModel = @$this->$relationAlias;
+		if (!$relationModel) {return;}
+		switch ($relation->getType()) {
+			case 0: // BelongsTo
+				$this->joinedBelongsToRelation($relationModel, $relationAlias);
+				break;
+			case 2: // HasMany
+				if (!$keyField) {
+					throw new Exception('If can use joinedRelation, you need to set '
+						. self::$referencesKeyOption . ' of relation ' . $relationAlias
+						. ' in model ' . get_class($this));
+				}
+				$this->joinedHasManyRelation($relationModel, $relationAlias, $keyField);
+				break;
+			case 1: // HasManyToMany
+				if (!$keyField) {return;}
+				throw new Exception('Sorry, relations with type HasManyToMany is not have implementations');
+				break;
+		}
+	}
+
+	/**
+	 * @param \Phalcon\Mvc\Model $relationModel
+	 * @param string $relationAlias
+	 * @return \Phalcon\Mvc\Model
+	 */
+	private function joinedBelongsToRelation(Model $relationModel, $relationAlias) {
+		return $this->joinedRelations[$relationAlias] = @$relationModel;
+	}
+
+	/**
+	 * @param \Phalcon\Mvc\Model[] $models
+	 * @param string $relationAlias
+	 * @param string $keyField
+	 */
+	private function joinedHasManyRelation($models, $relationAlias, $keyField) {
+		$this->joinedRelations[$relationAlias] = $models->filter(function($model) use ($keyField) {
+			$related = $model->getRelated($keyField);
+			return $related ? $related : array();
+		});
+	}
+
+	/**
 	 * Возвращает связь многие ко многим по alias.
 	 * @param $relationAlias
-	 * @return \Phalcon\Mvc\Model\Relation
+	 * @return Relation
 	 */
 	public function getHasManyRelation($relationAlias) {
 		/**
-		 * @var \Phalcon\Mvc\Model\Relation[] $relations
+		 * @var Relation[] $relations
 		 */
 		$relations = $this->getModelsManager()->getHasMany($this);
 		foreach ($relations as $relation) {
@@ -63,21 +128,28 @@ trait BaseModel
 	}
 
 	/**
-	 * Обращается и подгружает связанные данные сущности
-	 * @param $relation
+	 * Обращается и подгружает связанные данные сущности.
+	 * @param \Phalcon\Mvc\Model\Relation $relation
+	 * @param bool $isConvertToArray - Конвертировать ли в массив.
 	 * @return $this|void
 	 */
-	private function  assignRelation($relation) {
-		$options = $relation->getOptions();
-		$alias = @$options['alias'];
-
-		if (!$alias) {
-			return;
-		}
-		$data = $this->$alias;
-		$this->relations[$alias] = $data ? $data->toArray() : $data;
-
+	private function  assignRelation(Relation $relation, $isConvertToArray = true) {
+		$alias = $relation->getOption('alias');
+		$this->relations[$alias] = $this->getAssignRelation($this, $relation, $isConvertToArray);
 		return $this;
+	}
+
+	/**
+	 * @param \Phalcon\Mvc\Model $model
+	 * @param \Phalcon\Mvc\Model\Relation $relation
+	 * @param bool $isConvertToArray
+	 * @return array|Model\Resultset|\Phalcon\Mvc\Model|void
+	 */
+	private function getAssignRelation(Model $model, Relation $relation, $isConvertToArray = true) {
+		$alias = $relation->getOption('alias');
+		if (!$alias) {return;}
+		$data = $model->$alias;
+		return $data && $isConvertToArray ? $data->toArray() : $data;
 	}
 
 	/**
