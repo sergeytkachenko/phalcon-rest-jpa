048c9112713dcdc7301c18511236e0ea1c81ebe1
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2016-04-05 23:51:15 +0300

    #1943 added isNull in ppa

--- tests/ppa/rest/url/OperatorsUnitTest.php
+++ tests/ppa/rest/url/OperatorsUnitTest.php
@@ -88,7 +88,7 @@ class OperatorsUnitTest extends \UnitTestCase
 			),
 			'sql' => "SELECT `test_model`.`id`, `test_model`.`title`, `test_model`.`last_name` FROM `test_model` WHERE `test_model`.`title` LIKE :search OR `test_model`.`last_name` LIKE :search"
 		),
-		'/api/ppa/s/testModel/idIsNull' => array(
+		'/api/ppa/s/testModel/findByIdIsNull' => array(
 			'params' => array(),
 			'prepareParams' => array(),
 			'sql' => "SELECT `test_model`.`id`, `test_model`.`title`, `test_model`.`last_name` FROM `test_model` WHERE `test_model`.`id`  IS NULL"
