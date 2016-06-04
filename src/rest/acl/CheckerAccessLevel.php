6f18e2862d32ae65e09873909adfc160470aa709
Sergey Tkachenko <serg.tkachenko@hotmail.com>
2016-06-04 03:27:10 +0300

    #2145 service arh

--- src/rest/acl/CheckerAccessLevel.php
+++ src/rest/acl/CheckerAccessLevel.php
@@ -2,14 +2,15 @@
 namespace PPA\Rest\Acl;
 
 interface CheckerAccessLevel {
-	const DI_SERVICE_NAME = 'checkerAccessLevel'; 
+	const DI_SERVICE_NAME = 'checkerAccessLevel';
 	
 	/**
 	 * Checks access to crud operation.
 	 * array
 	 *  ['modelName'] string Name of the model.
 	 *  ['model'] \Phalcon\Mvc\Model Fetched model with data.
-	 *  ['action'] string Crud action name ('c' - create, 'r' - read, 'u' - update, 'd' - delete).
+	 *  ['action'] string Crud action name ('C' - create, 'R' - read, 'U' - update, 'D' - delete).
+	 *             Const from CrudOperations class.
 	 *  ['params'] array Fields name and fields values.
 	 * @param array $params
 	 * @return bool Return true if access is allowed, otherwise returns false.
